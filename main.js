90632bca2122785e7e79a66827ce018af39e7301
dashaknife <tvolos196@gmail.com>
2023-06-27 13:21:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,148 +1,260 @@
-'use strict';
+"use strict";
 
-let gl;                         // The webgl context.
-let surface;                    // A surface model
-let shProgram;                  // A shader program
-let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let gl; // The webgl context.
+let surface; // A surface model
+let shProgram; // A shader program
+let spaceball; // A SimpleRotator object that lets the user rotate the view by mouse.
+let texture;
+let stereoCamera;
 
-function deg2rad(angle) {
-    return angle * Math.PI / 180;
-}
+let webCamTexture, webCamVideo, webCamBackground;
 
+let handlePosition = 0.0;
+
+const texturePoint = { x: 100, y: 400 };
+
+let a = 0.5;
+let b = 1;
+
+const deg2rad = (angle) => {
+  return (angle * Math.PI) / 180;
+};
 
 // Constructor
 function Model(name) {
-    this.name = name;
-    this.iVertexBuffer = gl.createBuffer();
-    this.count = 0;
+  this.name = name;
+  this.iVertexBuffer = gl.createBuffer();
+  this.iTextureBuffer = gl.createBuffer();
+  this.count = 0;
 
-    this.BufferData = function(vertices) {
+  this.BufferData = function (vertices, textures) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iTextureBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textures), gl.STREAM_DRAW);
 
-        this.count = vertices.length/3;
-    }
+    gl.enableVertexAttribArray(shProgram.iTextureCoords);
+    gl.vertexAttribPointer(shProgram.iTextureCoords, 2, gl.FLOAT, false, 0, 0);
 
-    this.Draw = function() {
+    this.count = vertices.length / 3;
+  };
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-    }
+  this.Draw = function () {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+    gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iTextureBuffer);
+    gl.vertexAttribPointer(shProgram.iTextureCoords, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iTextureCoords);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
+  };
 }
-
 
 // Constructor
 function ShaderProgram(name, program) {
+  this.name = name;
+  this.prog = program;
+  // Location of the attribute variable in the shader program.
+  this.iAttribVertex = -1;
+  // Location of the uniform matrix representing the combined transformation.
+  this.iModelViewProjectionMatrix = -1;
+  this.iTextureCoords = -1;
+  this.iTextureU = -1;
 
-    this.name = name;
-    this.prog = program;
+  this.Use = function () {
+    gl.useProgram(this.prog);
+  };
+}
 
-    // Location of the attribute variable in the shader program.
-    this.iAttribVertex = -1;
-    // Location of the uniform specifying a color for the primitive.
-    this.iColor = -1;
-    // Location of the uniform matrix representing the combined transformation.
-    this.iModelViewProjectionMatrix = -1;
+const leftFrustum = (stereoCamera) => {
+  const { eyeSeparation, convergence, aspectRatio, fov, near, far } =
+    stereoCamera;
+  const top = near * Math.tan(fov / 2);
+  const bottom = -top;
 
-    this.Use = function() {
-        gl.useProgram(this.prog);
+  const a = aspectRatio * Math.tan(fov / 2) * convergence;
+  const b = a - eyeSeparation / 2;
+  const c = a + eyeSeparation / 2;
+
+  const left = (-b * near) / convergence;
+  const right = (c * near) / convergence;
+
+  return m4.orthographic(left, right, bottom, top, near, far);
+};
+
+const rightFrustum = (stereoCamera) => {
+  const { eyeSeparation, convergence, aspectRatio, fov, near, far } =
+    stereoCamera;
+  const top = near * Math.tan(fov / 2);
+  const bottom = -top;
+
+  const a = aspectRatio * Math.tan(fov / 2) * convergence;
+  const b = a - eyeSeparation / 2;
+  const c = a + eyeSeparation / 2;
+
+  const left = (-c * near) / convergence;
+  const right = (b * near) / convergence;
+  return m4.orthographic(left, right, bottom, top, near, far);
+};
+
+function draw() {
+  gl.clearColor(0,0,0,1);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  let modelView = spaceball.getViewMatrix();
+  let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0);
+  let matAccum = m4.multiply(rotateToPointZero, modelView);
+  let projection = m4.orthographic(0, 1, 0, 1, -1, 1);
+  let noRot = m4.multiply(
+    rotateToPointZero,
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+  );
+
+  const stereoCamera = {
+    eyeSeparation: parseFloat(document.getElementById("eyeSeparation").value),
+    convergence: parseFloat(document.getElementById("convergence").value),
+    aspectRatio: gl.canvas.width / gl.canvas.height,
+    fov: parseFloat(document.getElementById("fov").value),
+    near: parseFloat(document.getElementById("near").value),
+    far: 20.0,
+  };
+
+  let projectionLeft = leftFrustum(stereoCamera);
+  let projectionRight = rightFrustum(stereoCamera);
+
+  let translateToLeft = m4.translation(-0.03, 0, -20);
+  let translateToRight = m4.translation(0.03, 0, -20);
+
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, noRot);
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projection);
+  gl.bindTexture(gl.TEXTURE_2D, webCamTexture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    webCamVideo
+  );
+  webCamBackground?.Draw();
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.clear(gl.DEPTH_BUFFER_BIT);
+
+  let matAccumLeft = m4.multiply(translateToLeft, matAccum);
+  let matAccumRight = m4.multiply(translateToRight, matAccum);
+
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccumLeft);
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projectionLeft);
+  gl.colorMask(true, false, false, false);
+  surface.Draw();
+
+  gl.clear(gl.DEPTH_BUFFER_BIT);
+
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccumRight);
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projectionRight);
+  gl.colorMask(false, true, true, false);
+  surface.Draw();
+
+  gl.colorMask(true, true, true, true);
+}
+
+const step = (max, splines = 20) => {
+  return max / (splines - 1);
+};
+
+const cos = (x) => {
+  return Math.cos(x);
+};
+
+const sin = (x) => {
+  return Math.sin(x);
+};
+
+const CreateSurfaceData = () => {
+  let vertexList = [];
+    let textureList = [];
+    let splines = 20;
+
+    const n = 2;
+    const t = 0;
+    const POINTS = 60
+
+    let maxU = Math.PI;
+    let maxV = 2 * Math.PI;
+    let stepU = step(maxU, splines);
+    let stepV = step(maxV, splines);
+
+    let getU = (u) => {
+      return u / maxU;
+    };
+  
+    let getV = (v) => {
+      return v / maxV;
+    };
+
+    for (let u = 0; u <= POINTS; u += stepU) {
+
+      const r = u * 0.6 / POINTS + 0.4;
+
+      for (let v = 0; v <= POINTS; v += stepV) {
+
+        const fi = v * 10 * Math.PI / POINTS;
+
+        vertexList.push(
+          -(Math.cos(t + fi) / (2 * r)) - ((Math.pow(r, (1 + 2 * n)) * Math.cos(t - (1 + 2 * n) * fi)) / (2 + 4 * n)),
+          -(Math.sin(t + fi) / (2 * r)) - ((Math.pow(r, (1 + 2 * n)) * Math.sin(t - (1 + 2 * n) * fi)) / (2 + 4 * n)),
+          (Math.pow(r, n) * Math.cos(t - n * fi)) / n
+        );
+        textureList.push(getU(u), getV(v));
+        vertexList.push(
+          -(Math.cos(t + fi + stepV) / (2 * r)) - ((Math.pow(r, (1 + 2 * n)) * Math.cos(t - (1 + 2 * n) * fi + stepU)) / (2 + 4 * n)),
+          -(Math.sin(t + fi + stepV) / (2 * r)) - ((Math.pow(r, (1 + 2 * n)) * Math.sin(t - (1 + 2 * n) * fi + stepU)) / (2 + 4 * n)),
+          (Math.pow(r, n) * Math.cos(t - n * fi + stepV)) / n
+        );
+        textureList.push(getU(u + stepU), getV(v + stepV));
+      }
     }
-}
-
-
-/* Draws a colored cube, along with a set of coordinate axes.
- * (Note that the use of the above drawPrimitive function is not an efficient
- * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
- */
-function draw() { 
-    gl.clearColor(0,0,0,1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-    /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
-    /* Get the view matrix from the SimpleRotator object.*/
-    let modelView = spaceball.getViewMatrix();
-
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
-
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
-    /* Multiply the projection matrix times the modelview matrix to give the
-       combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
-
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
-
-    surface.Draw();
-}
-
-function CreateSurfaceData()
-{
-    let vertexList = [];
-
-    //fig #3
-    // const n = 2;
-    // const t = 0;
-    // const POINTS = 60
-
-
-     //fig #4
-     const n = 8;
-     const t = 0;
-     const POINTS = 30
-
-    for (let j = 0; j <= POINTS; j++) {
-
-        // fig #3
-        //const r = j * 1 / POINTS + 0.25;
-
-        // fig #4
-        const r = j * 0.6 / POINTS + 0.4;
-        
-
-        for (let i = 0; i <= POINTS; i++) {
-
-            const fi = i * 2 * Math.PI / POINTS;
-
-            const x = -(Math.cos(t + fi) / (2 * r)) - ((Math.pow(r, (1 + 2 * n)) * Math.cos(t - (1 + 2 * n) * fi)) / (2 + 4 * n))
-            const y = -(Math.sin(t + fi) / (2 * r)) - ((Math.pow(r, (1 + 2 * n)) * Math.sin(t - (1 + 2 * n) * fi)) / (2 + 4 * n))
-            const z = (Math.pow(r, n) * Math.cos(t - n * fi)) / n
-
-            vertexList.push(x, y, z);
-        }
-    }
-
-    return vertexList;
-}
-
+    return { vertexList, textureList };
+};
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+  let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-    shProgram = new ShaderProgram('Basic', prog);
-    shProgram.Use();
+  shProgram = new ShaderProgram("Basic", prog);
+  shProgram.Use();
 
-    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
-    shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+  shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+  shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(
+    prog,
+    "ModelViewProjectionMatrix"
+  );
+  shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
+  shProgram.iProjectionMatrix = gl.getUniformLocation(prog, "ProjectionMatrix");
 
-    surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
+  shProgram.iTextureCoords = gl.getAttribLocation(prog, "textureCoords");
+  shProgram.iTextureU = gl.getUniformLocation(prog, "textureU");
 
-    gl.enable(gl.DEPTH_TEST);
+  surface = new Model("Surface");
+  const { vertexList, textureList } = CreateSurfaceData();
+  surface.BufferData(vertexList, textureList);
+
+  webCamBackground = new Model("Background");
+  webCamBackground.BufferData(
+    [
+      0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0,
+      0.0, 0.0, 0.0,
+    ],
+    [1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1]
+  );
+
+  loadTexture();
+
+  gl.enable(gl.DEPTH_TEST);
 }
-
 
 /* Creates a program for use in the WebGL context gl, and returns the
  * identifier for that program.  If an error occurs while compiling or
@@ -153,56 +265,112 @@ function initGL() {
  * source code for the vertex shader and for the fragment shader.
  */
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader( gl.VERTEX_SHADER );
-    gl.shaderSource(vsh,vShader);
-    gl.compileShader(vsh);
-    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
-        throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
-    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
-    gl.shaderSource(fsh, fShader);
-    gl.compileShader(fsh);
-    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
-    }
-    let prog = gl.createProgram();
-    gl.attachShader(prog,vsh);
-    gl.attachShader(prog, fsh);
-    gl.linkProgram(prog);
-    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
-    }
-    return prog;
+  let vsh = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vsh, vShader);
+  gl.compileShader(vsh);
+  if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
+    throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
+  }
+  let fsh = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fsh, fShader);
+  gl.compileShader(fsh);
+  if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+    throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+  }
+  let prog = gl.createProgram();
+  gl.attachShader(prog, vsh);
+  gl.attachShader(prog, fsh);
+  gl.linkProgram(prog);
+  if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+    throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+  }
+  return prog;
 }
-
 
 /**
  * initialization function that will be called when the page has loaded
  */
 function init() {
-    let canvas;
-    try {
-        canvas = document.getElementById("webglcanvas");
-        gl = canvas.getContext("webgl");
-        if ( ! gl ) {
-            throw "Browser does not support WebGL";
-        }
-    }
-    catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not get a WebGL graphics context.</p>";
-        return;
-    }
-    try {
-        initGL();  // initialize the WebGL graphics context
-    }
-    catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
-        return;
-    }
+  let canvas;
+  try {
+    canvas = document.getElementById("webglcanvas");
+    gl = canvas.getContext("webgl");
 
-    spaceball = new TrackballRotator(canvas, draw, 0);
+    webCamVideo = document.createElement("video");
+    webCamVideo.setAttribute("autoplay", true);
+    webCamTexture = createWebCamTexture(gl);
 
-    draw();
+    getWebcam().then((stream) => (webCamVideo.srcObject = stream));
+
+    if (!gl) {
+      throw "Browser does not support WebGL";
+    }
+  } catch (e) {
+    console.log(e);
+    document.getElementById("canvas-holder").innerHTML =
+      "<p>Sorry, could not get a WebGL graphics context.</p>";
+    return;
+  }
+  try {
+    initGL(); // initialize the WebGL graphics context
+  } catch (e) {
+    console.log(e);
+    document.getElementById("canvas-holder").innerHTML =
+      "<p>Sorry, could not initialize the WebGL graphics context: " +
+      e +
+      "</p>";
+    return;
+  }
+
+  const eyeSeparationInput = document.getElementById("eyeSeparation");
+  const convergenceInput = document.getElementById("convergence");
+  const fovInput = document.getElementById("fov");
+  const nearInput = document.getElementById("near");
+
+  eyeSeparationInput.addEventListener("input", draw);
+  convergenceInput.addEventListener("input", draw);
+  fovInput.addEventListener("input", draw);
+  nearInput.addEventListener("input", draw);
+
+  spaceball = new TrackballRotator(canvas, draw, 0);
+
+  reDraw();
 }
+
+const reDraw = () => {
+  draw();
+  window.requestAnimationFrame(reDraw);
+};
+
+const loadTexture = () => {
+  const image = new Image();
+  image.crossOrigin = "anonymous";
+  image.src =
+    "https://www.the3rdsequence.com/texturedb/download/195/texture/jpg/1024/white+cracked+concrete-1024x1024.jpg";
+
+  image.addEventListener("load", () => {
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  });
+};
+
+const getWebcam = () => {
+  return new Promise((resolve) =>
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((s) => resolve(s))
+  );
+};
+
+const createWebCamTexture = () => {
+  let textureID = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, textureID);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  return textureID;
+};
